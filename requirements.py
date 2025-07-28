0bf5a798afa7d4866d42d237c36d8e6b425915c4
Brunno Henrique Damasceno Carmargo <03357343169@seguranca.local>
2025-07-28 13:10:49 -0400

    Versão inicial da API do BH1

--- requirements.py
+++ requirements.py
@@ -1,4 +1,16 @@
-ffastapi
-uvicorn
-google-cloud-firestore
-pydantic
+# H2OControl API
+
+API desenvolvida em Python com FastAPI para receber dados de sensores (ESP32) e armazenar no Firestore (Firebase).
+
+## 📦 Tecnologias
+- Python 3.10+
+- FastAPI
+- Google Cloud Firestore
+- Pydantic
+
+## 🚀 Como executar
+
+### 1. Instale as dependências
+
+```bash
+pip install -r requirements.txt
